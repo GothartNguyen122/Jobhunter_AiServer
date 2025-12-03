58fb3f6f23e6a82f93a2ec87cf28b64864e19467
GothartNguyen122 <117880525+GothartNguyen122@users.noreply.github.com>
2025-12-04 03:48:19 +0700

    a

--- config/index.js
+++ config/index.js
@@ -65,7 +65,9 @@ const config = {
       'http://127.0.0.1:5173',
       'http://127.0.0.1:8080',
       'http://127.0.0.1:5500',
-      'https://jobhunter-front-end-j5t3.vercel.app'
+      'https://jobhunter-front-end-j5t3.vercel.app',
+      'https://www.jobs-network.website',
+      'https://jobs-network.website'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
